a488b4b250e9d7722fbeaa7ecc059d4c163b35be
Nandadeep Davuluru <aditya.nandadeep@gmail.com>
2018-05-13 17:48:28 -0700

    refined

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ class Process (object):
 		self.id = pid
 		self.priority = priority
 
-		# flags state
+		# flags state dictionary
 		self.state = {'ready': False, 'blocked': False, 'running': False}
 
 		# metrics 
@@ -15,9 +15,14 @@ class Process (object):
 
 		# function that a process will execute
 		self.functionality = functionTodo
+		# result from executing the above function
+		self.results = None
 
+	# overriding the object printer for ease. Just do print(object) it spits out everything associated :)
 	def __str__(self):
 		return "Process ID - {}, Process priority - {}, Process State - {}, Start time - {}, End time - {}".format(self.id, self.priority, self.state, self.start_time, self.end_time)
+
+	# Getters and setters for the process object. 
 
 	def getPid(self):
 		return self.pid
@@ -37,15 +42,29 @@ class Process (object):
 	def set_endTime(self, time):
 		self.end_time = time
 
+	def set_state(self, state):
+		self.state[state] = True
+
+	# TODO. 
 	def launchProcess(self):
 		return self.functionality(self.id, self.priority, self.start_time, self.end_time, self.state)
 
+# Example function calls associated to a process object. Need to add timers to capture metrics. 
 def readFile(file):
 	with open(file, 'r') as f:
 		print(f.read())
 
+# add = lambda x,y : x + y
+def add(a,b):
+	print(a + b)
+
 # an example usage, it can be substituted with other function calls. - NEEDS END TO END TESTING. 
-process = Process(1, "High", 0, 5, 0, readFile('carseats.csv'))
+process = Process(1, "High", 0, 5, 0, add(1, 3))
+print(process)
+
+process.set_state("running")
+# this is the example for overriding __str__ function 
+print(process)
 
 
 

a3ff04c01347eda02f6d162db3e08f323f8d61a5
Tom Salata <tomsalata@gmail.com>
2018-05-20 13:05:19 -0700

    Simulator init. Renamed round-robin.py to round_robin.py in line with python naming conventions. Added __init__.py to treat shedulers dir as containing packages.

---
+++ simulator.py
@@ -0,0 +1,44 @@
+"""
+#====================================================================
+Process Simulator
+
+Create a list of process to run simulations of three different
+schedulers.
+#====================================================================
+"""
+
+import copy
+import process
+from schedulers import *
+
+class Simulator:
+    def __init__(self, procs, sched):
+        # make a deep copy of all processes
+        self.processList = copy.deepcopy(procs)
+        if sched == "fifo":
+            self.scheduler = fifo.FIFO()
+        elif sched == "round-robin":
+            self.scheduler = round_robin.RR()
+        else:
+            self.scheduler = cfs.CFS()
+        # add processes to scheduler
+        for i in self.processList:
+            self.scheduler.addProcess(i)
+
+    def run(self):
+        self.scheduler.run()
+
+# Create array of different processes to be used by each scheduler
+processes = []
+# TODO: create list of different processes
+processes.append(process.Process(1, "High", 0, 5, 0, 1))
+
+# Create simulators for each scheduler
+fifo = Simulator(processes, "fifo")
+rr = Simulator(processes, "round-robin")
+cfs = Simulator(processes, "cfs")
+
+# Run each scheduler simulation
+fifo.run()
+rr.run()
+cfs.run()

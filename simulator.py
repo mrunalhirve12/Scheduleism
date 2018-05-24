c952319be08ff39ca78a19525c93a50629760c05
Tom Salata <tomsalata@gmail.com>
2018-05-23 20:34:08 -0700

    On context switches to new process an extra cost is added. This demonstrates how Round-Robin reduces latency before a process runs, but results in added overhead from needing additional context switches.

--- simulator.py
+++ simulator.py
@@ -11,8 +11,7 @@ from collections import deque
 
 import process
 
-from schedulers import fifo
-from schedulers import round_robin
+from schedulers import *
 
 from defines import COMPLETE
 from defines import INCOMPLETE
@@ -48,12 +47,14 @@ def main():
     # Add a process to processQ with a process id, priority, time for completion,
     # and time to start the process relative to systemTime
     # Note: must add processes in sorted order by start time
-    processQ.append(process.Process(1, "High", 20, 0))
-    processQ.append(process.Process(2, "Low", 20, 11))
+    processQ.append(process.Process(1, "High", 100, 0))
+    processQ.append(process.Process(2, "Low", 200, 11))
+    processQ.append(process.Process(3, "High", 100, 0))
 
     simulator(processQ, scheduler, timerInterrupt)
 
 def simulator(processQ, scheduler, timerInterrupt):
+    prevProc = None
     curProc = None
     systemTime = 0
     procTime = 0
@@ -69,11 +70,15 @@ def simulator(processQ, scheduler, timerInterrupt):
         # entered via timer interrupt or no user process running
         if procTime == timerInterrupt or curProc is None or \
             (curProc is not None and curProc.get_status() == COMPLETE):
-            # run the scheduler
-            # TODO: impose extra time cost when switching to a new user process
-            curProc = scheduler.get_next(curProc)
-            procTime = 0
             print("EXEC:       0  at ",systemTime)
+            # run the scheduler
+            # impose extra time cost when switching to a new user process
+            curProc = scheduler.get_next(curProc)
+            if prevProc != curProc and curProc is not None:
+                systemTime += 1        
+                print("CTXT SWTCH:",curProc.getPid()," at ",systemTime)
+                prevProc = curProc
+            procTime = 0
 
         # simulated user space execution
         elif curProc is not None:

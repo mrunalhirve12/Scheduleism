c5c83845b8979100e69f5234e23701346069dc3d
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2018-05-29 13:54:48 -0700

    Random process generator done

--- simulator.py
+++ simulator.py
@@ -2,26 +2,53 @@
 #====================================================================
 Process Simulator
 
-Execute: python3 simulator.py ['fifo','rr', 'cfs'] [timer interrupt value]
+Execute: python3 simulator.py ['fifo','rr', 'cfs'] [timer interrupt value] [number of process to generate]
 #====================================================================
 """
 
 import sys
+import random
 import process
 from schedulers import fifo, round_robin as rr, cfs
 from collections import deque
+from defines import START_TIME_START, START_TIME_END, BURST_TIME_START, BURST_TIME_END
+
+#==============================================
+#This function randomly generates a specified 
+#number of processes
+#Params:
+#   num = the number of processes to create.
+#         Default is 100
+#Return:
+#   processQ = the deque of processes sorted by
+#              process start time
+#==============================================
+def generateProcess(num = 100):
+    processQ = list()
+    priorities = ["High", "Low"]
+
+    #Create random processes
+    for i in range(1, num + 1):
+        processQ.append(process.Process(i, 
+                                        random.choice(priorities), 
+                                        random.randint(BURST_TIME_START, BURST_TIME_END), 
+                                        random.randint(START_TIME_START, START_TIME_END)
+                                       ))
+
+    #Sort the process queue by start time in ascending order
+    processQ = sorted(processQ, key=lambda process: process.get_startTime())
+    
+    for p in processQ:
+        print(p)
+    
+    return deque(processQ)
 
 def main():
+    #Get the timer interrupt from user
     timerInterrupt = int(sys.argv[2])
-    # TODO: import processes from random process generator 
-    # process queue in the order in which to add
-    processQ = deque([])
-    # Add a process to processQ with a process id, priority, time for completion,
-    # and time to start the process relative to systemTime
-    # Note: must add processes in sorted order by start time
-    processQ.append(process.Process(1, "High", 100, 0))
-    processQ.append(process.Process(2, "High", 100, 0))
-    processQ.append(process.Process(3, "Low", 200, 11))
+
+    #Generate random processes
+    processQ = generateProcess(int(sys.argv[3]))
 
     if sys.argv[1].casefold() == "fifo".casefold():
         print("SCHEDULER: FIFO")

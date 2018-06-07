7ac0ab503176efc198842f42b3bbbd32b552c039
Manpreet Bahl <manpreet@fiordland.cs.pdx.edu>
2018-06-07 14:07:04 -0700

    Started flask web app

---
+++ app.py
@@ -0,0 +1,34 @@
+#======================Imports========================
+from flask import Flask, render_template, request, redirect, url_for
+from simulator import start_simulation
+#=====================================================
+
+#======================Setup==========================
+app = Flask(__name__)
+#=====================================================
+
+
+#====================Home Route=======================
+@app.route("/")
+def home():
+    return render_template('home.html')
+#=====================================================
+
+
+#================Submit Review========================
+@app.route("/simulate", methods=['POST'])
+def simulate():
+    try:
+        start_simulation(int(request.form["timer"]), int(request.form["processNum"]))
+        return redirect(url_for('home'))
+    except Exception as e:
+        print("Error encountered: " + str(e)) 
+#=====================================================
+
+
+#=================Start Server========================
+app.debug = True
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+#=====================================================
+
